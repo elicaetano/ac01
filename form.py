b0cf337f53daad471d0c0a057eccb286364e84be
elicaetano <100229725+elicaetano@users.noreply.github.com>
2022-09-26 18:01:08 -0300

    Update form.py

--- form.py
+++ form.py
@@ -14,6 +14,7 @@ app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
 app.config['MYSQL_DATABASE_DB'] = 'produto'
 app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['DB_SERVICE'] = '172.17.0.16'
 mysql.init_app(app)
 
 @app.route('/')
