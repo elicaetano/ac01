210bd6b6ee72f487e8c8f0e4e807c73d4f91ff3d
elicaetano <100229725+elicaetano@users.noreply.github.com>
2022-09-26 16:47:03 -0300

    Update form.py

--- form.py
+++ form.py
@@ -3,27 +3,44 @@
 import os
 from flask import Flask
 from flask import request, render_template
-from flask_sqlalchemy import SQLAlchemy
-from config import BaseConfig
+from flaskext.mysql import MySQL
 
 
+mysql = MySQL()
 app = Flask(__name__)
-app.config.from_object(BaseConfig)
-db = SQLAlchemy(app)
 
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
+app.config['MYSQL_DATABASE_DB'] = 'produto'
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+mysql.init_app(app)
 
-from models import *
+@app.route('/')
+def main():
+    return render_template('formulario.html')
 
+@app.route('/gravar', methods=['POST','GET'])
+def gravar():
+  nome = request.form['nome']
+  preco = request.form['preco']
+  categoria = request.form['categoria']
+  if nome and preco and categoria:
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute('insert into produtos_unidade (nome,preco,categoria) VALUES (%s, %s, %s)', (nome, preco, categoria))
+    conn.commit()
+  return render_template('formulario.html')
 
 @app.route('/', methods=['GET', 'POST'])
-def formulario():
-    if request.method == 'POST':
-        text = request.form['text']
-        post = Post(text)
-        db.session.add(post)
-        db.session.commit()
-    posts = Post.query.order_by(Post.date_posted.desc()).all()
-    return render_template('formulario.html', posts=posts)
+def listar():
+  conn = mysql.connect()
+  cursor = conn.cursor() #abre uma seção dentro da conexão
+  cursor.execute('select nome, preco, categoria FROM produtos')
+  data = cursor.fetchall() #recuperar registros
+  conn.commit()
+  return render_template('lista.html', datas=data)
+
 
 
 
